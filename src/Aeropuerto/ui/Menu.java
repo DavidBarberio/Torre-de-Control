52592165f172cea1428c5e9f6d877d3ef1ec56b3
David <david_barberio@hotmail.com>
2022-05-19 20:27:11 +0200

    Limpiar código

--- src/Aeropuerto/ui/Menu.java
+++ src/Aeropuerto/ui/Menu.java
@@ -41,7 +41,7 @@ public class Menu extends JFrame {
 		opcion2 = new JButton("2.- Solicitar permiso para despegue");
 		opcion2.addActionListener(e -> seleccionarDespegue());
 		
-		opcion3 = new JButton("3.- Autorizar Operaci�n");
+		opcion3 = new JButton("3.- Autorizar Operación");
 		opcion3.addActionListener(e -> seleccionarAutorizar());
 		
 		opcion4 = new JButton("4.- Historial Operaciones");
